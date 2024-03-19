5c195f09fae2aa6501d6d43da72a9f3557943e23
Anna <missjellx@gmail.com>
2024-03-19 23:50:44 +0500

    add counter

--- github-clone/src/api/octokit/index.ts
+++ github-clone/src/api/octokit/index.ts
@@ -1,5 +1,3 @@
 import { Octokit } from "octokit";
 
-export const octokit = new Octokit({
-  auth: "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",
-});
+
